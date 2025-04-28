71295185d970329173b7ccb7edd4b5e5cba73280
Nthabi <nthabimashiane@gmail.com>
2025-04-28 15:36:22 +0200

    added crud operations

--- src/app/reservation/reservation.service.ts
+++ src/app/reservation/reservation.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { Reservation } from '../models/reservation';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReservationService {
 
+  private reservations: Reservation[] = [];
+
   constructor() { }
+
+  getReservations(): Reservation[] {
+    return this.reservations;
+  }
+
+  getReservation(id: string): Reservation | undefined {
+    return this.reservations.find(res => res.id === id)
+  }
+
+  addReservation(reservation: Reservation) {
+    this.reservations.push(reservation);
+  }
+
+  deleteReservation(id: string) {
+    let index = this.reservations.findIndex(res => res.id === id);
+    this.reservations.splice(index, 1);
+  }
+
+  updateReservation(reservation: Reservation) {
+    let index = this.reservations.findIndex(res => res.id === reservation.id);
+    this.reservations[index] = reservation;
+  }
 }
