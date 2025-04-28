29f0593a5abffedf5a74620ee63e1baac13f2884
Nthabi <nthabimashiane@gmail.com>
2025-04-28 15:08:28 +0200

    added simple form validation

--- src/app/reservation/reservation.component.ts
+++ src/app/reservation/reservation.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reservation',
   templateUrl: './reservation.component.html',
   styleUrl: './reservation.component.css'
 })
-export class ReservationComponent {
+export class ReservationComponent implements OnInit {
+ 
 
+  //formgroup we will bind to our form
+  reservationForm: FormGroup = new FormGroup({});
+
+  constructor(private formBuilder: FormBuilder) {
+
+  }
+
+  ngOnInit(): void {
+    this.reservationForm = this.formBuilder.group({
+      checkInDate: ['', Validators.required],
+      checkOutDate: ['', Validators.required],
+      guestName: ['', Validators.required],
+      guestEmail: ['', [Validators.required, Validators.email]],
+      roomNumber: ['', Validators.required]
+    })
+  }
+
+  onSubmit() {
+    if(this.reservationForm.valid) {
+      console.log('valid');
+    } else {
+      console.log('Invalid');
+    }
+  }
 }
